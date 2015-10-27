d49b94d65122090e0e8ddcc7f90e5b9c5f8b166d
billy7907 <billy7907@gmail.com>
2015-10-27 16:37:45 -0700

    Added script and README files

---
+++ newproject.sh
@@ -0,0 +1,20 @@
+# Billy Hung, 2015-10-26
+# Make a project directory 
+# Usage: Create a project directory (doc, data, src, bin, results) according to Nobel,2009, and adding LICENSE.md into the root directroy. Then adding README.md into each directory
+
+mkdir -p new_project/{doc,data,src,bin,results,LICENSE.md}
+cd new_project
+cd doc
+touch README.md
+cd ..
+cd data
+touch README.md
+cd ..
+cd src
+touch README.md
+cd ..
+cd bin
+touch README.md
+cd ..
+cd results
+touch README.md
