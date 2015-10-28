f046be1688882c845e9dd4998af5e8345202ff26
kwfawkes <kyle.fawkes@gmail.com>
2015-10-27 18:08:23 -0700

    completed README.md in root directory as well as LICENSE.md

--- project.directory.sh
+++ project.directory.sh
@@ -8,8 +8,28 @@ Generic Project Directory, project.directory.sh
 Kyle Fawkes
 2015-10-27" > $1/README.md
 
-#Creating a LICENSE.md documentation file directly under the root directory
-touch $1/LICENSE.md
+#Creating a LICENSE.md documentation file directly under the root directory, which includes a GNU General Public License agreement
+echo "General Project Directory
+    Copyright (C) 2015  Kyle Fawkes
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Python  Copyright (C) 2015  Kyle Fawkes
+
+    This program comes with ABSOLUTELY NO WARRANTY; for details type 'show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type 'show c' for details." > $1/LICENSE.md
 
 #Creating an organizational framework for the project with 4 main directories, 5th directory "results" added later 
 mkdir -p $1/{doc,data,src,bin,results}
