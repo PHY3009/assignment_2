a5d3da22a234e8eca20ca8c980dd7e7efa754332
billy7907 <billy7907@gmail.com>
2015-10-27 18:11:35 -0700

    improved script

--- newproject.sh
+++ newproject.sh
@@ -6,15 +6,20 @@ mkdir -p new_project/{doc,data,src,bin,results,LICENSE.md}
 cd new_project
 cd doc
 touch README.md
+echo "for manuscript" > README.md
 cd ..
 cd data
 touch README.md
+echo "for datasets" > README.md
 cd ..
 cd src
 touch README.md
+echo "for codes" > README.md
 cd ..
 cd bin
 touch README.md
+echo "for scripts" > README.md
 cd ..
 cd results
 touch README.md
+echo "for statistical results" > README.md
